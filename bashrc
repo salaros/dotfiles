ff4692294adf2fc92b47e80742be71ea05dc0dc4
salaros <salaros@yandex.ru>
2017-10-06 23:34:58 +0300

    huge improvement to the installation script
    removed dot char ('.') prefix from dotfiles names, so they are visible via file explorer etc
    renamed bash_aliases to just aliases
    smart backup / removal of existing dotfiles

--- bashrc
+++ bashrc
@@ -3,18 +3,18 @@
 # If not running interactively, don't do anything
 case $- in
     *i*) ;;
-      *) return;;
+        *) return;;
 esac
 
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
 if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
+    if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+    elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+    fi
 fi
 
 # Set some bash options and apply some tweaks
@@ -24,7 +24,7 @@ fi
 [[ -f ~/.bash_completion ]] && . ~/.bash_completion
 
 # Alias definitions
-[[ -f ~/.bash_aliases ]] && . ~/.bash_aliases
+[[ -f ~/.aliases ]] && . ~/.aliases
 
 # Coloring the bash here
 [[ -f ~/.bash_prompt ]] && . ~/.bash_prompt
--- install.sh
+++ install.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
 
+nocolor=$'\e[0m'
+bold=$(tput bold)                         # make colors bold/bright
+red="$bold$(tput setaf 1)"                # bright red text
+green=$(tput setaf 2)                     # dim green text
+fawn=$(tput setaf 3); beige="$fawn"       # dark yellow text
+yellow="$bold$fawn"                       # bright yellow text
+darkblue=$(tput setaf 4)                  # dim blue text
+blue="$bold$darkblue"                     # bright blue text
+purple=$(tput setaf 5); magenta="$purple" # magenta text
+pink="$bold$purple"                       # bright magenta text
+darkcyan=$(tput setaf 6)                  # dim cyan text
+cyan="$bold$darkcyan"                     # bright cyan text
+gray=$(tput setaf 7)                      # dim white text
+darkgray="$bold"$(tput setaf 0)           # bold black = dark gray text
+white="$bold$gray"                        # bright white text
+
+# Create a backup folder for existing configs
+if [ ! -d ~/.var/backup ]; then
+	mkdir -pv ~/.var/backup/dotfiles
+fi
+
+#
+echo "${red}Backuping existent Bash configs (removing symlinks)${nocolor}"
+for file in ~/.{aliases,dircolors,bash_completion,bashrc,bash_prompt,bash_tweaks,profile}; do
+    if [ -f $file ] && [ -L $file ]; then
+        rm -fv $file
+    elif [ -f $file ]; then
+        mv -v $file ~/.var/backup/dotfiles
+    fi
+done;
+printf "${red}Done!${nocolor}\n\n"
+
 # Install Bash-related files via symlinks
-ln -sv $(pwd)/.bash* ~/
+echo "${purple}Installing new Bash configs${nocolor}"
+for file in $(pwd)/{aliases,dircolors,bash_completion,bashrc,bash_prompt,bash_tweaks,profile}; do
+	ln -sv "$file" "$HOME/.$(basename $file)"
+done;
+printf "${purple}Done!${nocolor}\n\n"
+
+source ~/.bashrc && exec bash
